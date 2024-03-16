54ecd43c5d676e1c8edda3cccb16ff96d61aa8d7
vinycius <vinycius.dasilva@jala.university>
2024-03-16 11:31:43 -0300

    First commit

---
+++ src/main/java/com/Jala/B2Laboratorio2/B2Laboratorio2Application.java
@@ -0,0 +1,13 @@
+package com.Jala.B2Laboratorio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2Laboratorio2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B2Laboratorio2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/Jala/B2Laboratorio2/B2Laboratorio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jala.B2Laboratorio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2Laboratorio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
